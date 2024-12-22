c4ab11bd3aab730bf6da4ec4aa7ecff595c4641a
Regis-tan <144893732+Regis-tan@users.noreply.github.com>
2024-12-22 12:46:42 +0700

    Update Database_SQL_Code.sql

--- Database_SQL_Code.sql
+++ Database_SQL_Code.sql
@@ -6,76 +6,76 @@ USE ums_db;
 
 -- Creates tables
 CREATE TABLE department(
-	depId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    depName varchar(50) NOT NULL
+	departmentId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    departmentName varchar(50) NOT NULL
 );
 
 CREATE TABLE course(
-	crsId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    crsName varchar(100) NOT NULL,
+	courseId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    courseName varchar(100) NOT NULL,
     credit int
 );
 
 CREATE TABLE program(
-	prgId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    prgName varchar(50) NOT NULL,
-    depId int NOT NULL,
+	programId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    programName varchar(50) NOT NULL,
+    departmentId int NOT NULL,
     degree varchar(50) NOT NULL,
     
-    FOREIGN KEY (depId) REFERENCES department(depId)
+    FOREIGN KEY (departmentId) REFERENCES department(departmentId)
 );
 
 CREATE TABLE staff(
-	staId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    depId int NOT NULL,
-    `name` varchar(100) NOT NULL,
+	staffId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    departmentId int NOT NULL,
+    staffName varchar(100) NOT NULL,
     position varchar(50),
     DOB date NOT NULL,
     gender char(1),
     phoneNo varchar(20),
     
-    FOREIGN KEY (depId) REFERENCES department(depId)
+    FOREIGN KEY (departmentId) REFERENCES department(departmentId)
 );
 
 CREATE TABLE class(
-	clsId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	staId int NOT NULL,
-    crsId int NOT NULL,
+	classId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	staffId int NOT NULL,
+    courseId int NOT NULL,
     
-    FOREIGN KEY (staId) REFERENCES staff(staId),
-    FOREIGN KEY (crsId) REFERENCES course(crsId)
+    FOREIGN KEY (staffId) REFERENCES staff(staffId),
+    FOREIGN KEY (courseId) REFERENCES course(courseId)
 );
 
 CREATE TABLE student(
-	stuId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name` varchar(100) NOT NULL,
+	studentId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    studentName varchar(100) NOT NULL,
     gender char(1) NOT NULL,
-    prgId int,
+    programId int,
     DOB date NOT NULL,
     entryDate date NOT NULL,
     phoneNo varchar(20),
     email varchar(320),
+    gpa double,
     
-    FOREIGN KEY (prgId) REFERENCES program(prgId)
+    FOREIGN KEY (programId) REFERENCES program(programId)
 );
 
 CREATE TABLE enrollment(
-	enrId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    clsId int NOT NULL,
-    stuId int NOT NULL,
-    enrDate date NOT NULL,
-    grade int,
+	enrollId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    classId int NOT NULL,
+    studentId int NOT NULL,
+    enrollDate date NOT NULL,
+    enrollAvailable date NOT NULL,
     
-    FOREIGN KEY (clsId) REFERENCES class(clsId),
-    FOREIGN KEY (stuId) REFERENCES student(stuId)
+    FOREIGN KEY (classId) REFERENCES class(classId),
+    FOREIGN KEY (studentId) REFERENCES student(studentId)
 );
 
 CREATE TABLE `schedule`(
-	schId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    enrId int NOT NULL,
+	scheduleId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    enrollId int NOT NULL,
     `day` varchar(20) NOT NULL,
     `time` time NOT NULL,
     
-    FOREIGN KEY (enrId) REFERENCES enrollment(enrId)
+    FOREIGN KEY (enrollId) REFERENCES enrollment(enrollId)
 );
-
